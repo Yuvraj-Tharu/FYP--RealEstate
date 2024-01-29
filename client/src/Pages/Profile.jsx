1c8946f6c7143a52b65f5e04442b543d2632b062
November414241 <yuvrajtharu123@gmail.com>
2024-01-29 16:26:20 +0545

    -using Redux Persist to store the user data

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
+  // console.log(currentUser.result.avatar);
+
   return (
-    <div>
+    <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile page</h1>
-      <form action="">
-        <img src="" alt="" />
+      <form className="flex flex-col">
+        <img
+          src={currentUser.result.avatar}
+          alt=""
+          className="rounded-full h-24 w-24 object-cover self-center mt-2"
+        />
+
+        <input className="border p-3 rounded-lg" type="text" placeholder="" />
       </form>
     </div>
   );

fee8122f6de42b9ecab2047a7f481a28e9a0d6dc
November414241 <yuvrajtharu123@gmail.com>
2024-01-30 10:22:27 +0545

    -complete profile page UI

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -8,15 +8,43 @@ export default function Profile() {
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile page</h1>
-      <form className="flex flex-col">
+      <form className="flex flex-col gap-4">
         <img
           src={currentUser.result.avatar}
           alt=""
           className="rounded-full h-24 w-24 object-cover self-center mt-2"
         />
 
-        <input className="border p-3 rounded-lg" type="text" placeholder="" />
+        <input
+          className="border p-3 rounded-lg"
+          type="text"
+          id="username"
+          placeholder="First Name"
+          required
+        />
+        <input
+          className="border p-3 rounded-lg "
+          type="text"
+          placeholder="Last Name"
+          required
+        />
+        <input
+          className="border p-3 rounded-lg "
+          id="email"
+          type="email"
+          placeholder="Email"
+          required
+        />
+
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95">
+          update
+        </button>
       </form>
+
+      <div className="flex justify-between mt-5">
+        <span className="text-red-600 cursor-pointer">Delete Account</span>
+        <span className="text-red-600 cursor-pointer">Sign out</span>
+      </div>
     </div>
   );
 }

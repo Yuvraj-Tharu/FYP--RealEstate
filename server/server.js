0a157b20ae269f52c80c402b485cc4ad1f3e8f4e
November414241 <yuvrajtharu123@gmail.com>
2024-08-07 23:57:41 +0545

    fixing the deployment path

--- server/server.js
+++ server/server.js
@@ -49,8 +49,8 @@ app.use("/", MessageRouter);
 app.use("/", MsgRoutes);
 app.use("/", auctionRoutes);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
-// app.use(express.static(path.join(__dirname, "client", "dist")));
+// app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "client", "dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });

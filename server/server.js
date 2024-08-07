e160dc85f245e264b7a22aaa1c7b8035bfaccda7
November414241 <yuvrajtharu123@gmail.com>
2024-08-08 01:48:11 +0545

    chnage the dirname

--- server/server.js
+++ server/server.js
@@ -6,15 +6,11 @@ require("dotenv").config();
 const userRouter = require("./Routes/user.routes");
 const loginRouter = require("./Routes/user.loginRoutes");
 const ResetPasswordRouter = require("./Routes/resetpassword.routes");
-// const AdminLoginRoutes = require("./Routes/AdminLogin.routes");
 const UserProfileUpdateRouter = require("./Routes/userProfile.routes");
 const createListingRouter = require("./Routes/createListing.routes");
-// const getListingRouter = require("./Routes/createListing.routes");
 const deleteRouter = require("./Routes/deleteListing.routes");
-
 const getUserRouter = require("./Routes/getUser.routes");
 const searchRoutes = require("./Routes/searchListing.routes");
-
 const MessageRouter = require("./Routes/Message.Routes");
 
 // ?
@@ -25,7 +21,7 @@ const auctionRoutes = require("./Routes/Auction.Routes");
 app.use(express.json());
 app.use(cors());
 const path = require("path");
-const __dirname = path.resolve();
+const dirname = path.resolve();
 
 const port = process.env.PORT || 3000;
 
@@ -55,10 +51,10 @@ app.use("/", auctionRoutes);
 //   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 // });
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(dirname, "client", "dist", "index.html"));
 });
 
 app.listen(port, () => {

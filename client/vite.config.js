9bf8a243416dae1087daf1702d51277c082893e2
November414241 <yuvrajtharu123@gmail.com>
2024-08-13 15:33:27 +0545

    sss

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://deploy-realestate.onrender.com",
         secure: false,
       },
     },
